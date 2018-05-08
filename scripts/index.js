6970b6d78d2ed4e2fc29e24cf0c1d1a91f9f43f3
Adam Barry-O'Donovan <adambarryodonovan@gmail.com>
2018-05-08 23:18:33 +0100

    Background rises and falls with the tide

--- scripts/index.js
+++ scripts/index.js
@@ -7,38 +7,57 @@ $(document).ready (() => {
             latitude = data.coords.latitude;
         });
     }
+    else {
+        $('.location').html ('Location unavailable')
+        console.log ()
+    }
 
     dateTime = new Date();
     //document.querySelector ('time').innerHTML = dateTime
-    $('.time').html (`${dateTime.getDate()}/${dateTime.getMonth()}/${dateTime.getFullYear()} ${dateTime.getHours()}:${dateTime.getMinutes()}`)
+    $('.time').html (`${dateTime.getDate()}/${dateTime.getMonth()}/${dateTime.getFullYear()} ${dateTime.getHours()}:${(dateTime.getMinutes()<10?'0':'') + dateTime.getMinutes()}`)
 
     $.ajax({
         url: `https://www.worldtides.info/api?extremes&lat=${latitude}&lon=${longitude}&key=611e7ad2-9684-49e0-ac23-8875a5f7f218`
       }).done(function(data) {
+          console.log (data)
             if (data.extremes[0].type == 'High') {
                 //document.querySelector ('highTide').innerHTML = data.extremes[0].date;
-                dateTime = new Date (data.extremes[0].date)
-                $('.highTide').html (dateTime.getHours() + 1 + ":" + (dateTime.getMinutes()<10?'0':'') + dateTime.getMinutes());
+                highTideTime = new Date (data.extremes[0].date)
+                $('.highTide').html (highTideTime.getHours() + 1 + ":" + (highTideTime.getMinutes()<10?'0':'') + highTideTime.getMinutes());
             }
             else {
                 //document.querySelector ('lowTide').innerHTML = data.extremes[0].date;
-                dateTime = new Date (data.extremes[0].date)
-                $('.lowTide').html (dateTime.getHours() + 1 + ":" + (dateTime.getMinutes()<10?'0':'') + dateTime.getMinutes());
+                lowTideTime = new Date (data.extremes[0].date)
+                $('.lowTide').html (lowTideTime.getHours() + 1 + ":" + (lowTideTime.getMinutes()<10?'0':'') + lowTideTime.getMinutes());
             }
 
             if (data.extremes[1].type == 'Low') {
                 //document.querySelector ('lowTide').innerHTML = data.extremes[1].date;
-                dateTime = new Date (data.extremes[1].date)
-                $('.lowTide').html (dateTime.getHours() + 1 + ":" + (dateTime.getMinutes()<10?'0':'') + dateTime.getMinutes());
+                lowTideTime = new Date (data.extremes[1].date)
+                $('.lowTide').html (lowTideTime.getHours() + 1 + ":" + (lowTideTime.getMinutes()<10?'0':'') + lowTideTime.getMinutes());
             }
             else {
                 //document.querySelector ('highTide').innerHTML = data.extremes[1].date;
-                dateTime = new Date (data.extremes[1].date)
-                $('.highTide').html (dateTime.getHours() + 1 + ":" + (dateTime.getMinutes()<10?'0':'') + dateTime.getMinutes());
+                highTideTime = new Date (data.extremes[1].date)
+                $('.highTide').html (highTideTime.getHours() + 1 + ":" + (highTideTime.getMinutes()<10?'0':'') + highTideTime.getMinutes());
             }
+
+            calculateHeight (new Date (), lowTideTime, highTideTime);
         });
 })
 
-function calculateHeight (currentTime, nextHighTide) {
-    
+function calculateHeight (currentTime, nextLowTide, nextHighTide) {
+    if (nextLowTide < currentTime) {
+        console.log ("Low tide is less");
+        currentTideTime = Math.abs(currentTime - lowTideTime);
+        percentage = (currentTideTime / 22200000) * 100;
+        $('.data').css ('background', `linear-gradient(to top, #5f9ecf ${percentage}%, cadetblue ${100 - percentage}%)`);
+    }
+    else if (nextLowTide > currentTime && currentTime > nextHighTide) {
+        console.log ("High tides");
+        currentTideTime = Math.abs(currentTime - highTideTime);
+        percentage = (currentTideTime / 22200000) * 100;
+        console.log (percentage)
+        $('.data').css ('background', `linear-gradient(to top, #5f9ecf ${percentage}%, cadetblue ${percentage}%, cadetblue ${100 - percentage}%)`);
+    }
 }
