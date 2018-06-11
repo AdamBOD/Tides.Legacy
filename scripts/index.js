c6c78baedc56f1016ff00f915b8907109d8f7132
AdamBOD <adambarryodonovan@gmail.com>
2018-06-11 08:57:27 +0100

    Fixed location service

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,7 @@ $(document).ready (() => {
             console.log (`Longitude: ${longitude} Latitude: ${latitude}`);
             
             $.ajax ({
-                url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=53.3603142,-6.3150542000000005&key=AIzaSyAC148-4ycwCA5I7sc7TXISife7BObCRuk',
+                url: `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyAC148-4ycwCA5I7sc7TXISife7BObCRuk`,
                 success: (data) => {
                     console.log (data);
                     $('.location').html (data.results[3].formatted_address);
