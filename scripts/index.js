ba2c41dd17a0b2e185c546f0f147530d7c5d862c
AdamBOD <adambarryodonovan@gmail.com>
2018-06-15 09:13:18 +0100

    Fixed time formatting

--- scripts/index.js
+++ scripts/index.js
@@ -26,56 +26,23 @@ $(document).ready (() => {
                 url: `https://www.worldtides.info/api?extremes&lat=${latitude}&lon=${longitude}&key=611e7ad2-9684-49e0-ac23-8875a5f7f218`,
                 success: (data) => {
                             console.log (data)
-                            // if (data.extremes[0].date != NaN) {
-                            //     if (data.extremes[0].type == 'High') {
-                            //         //document.querySelector ('highTide').innerHTML = data.extremes[0].date;
-                            //         highTideTime = new Date (data.extremes[0].date)
-                            //         $('.highTide').html (highTideTime.getHours() + 1 + ":" + (highTideTime.getMinutes()<10?'0':'') + highTideTime.getMinutes());
-                            //     }
-                            //     else {
-                            //         //document.querySelector ('lowTide').innerHTML = data.extremes[0].date;
-                            //         lowTideTime = new Date (data.extremes[0].date)
-                            //         $('.lowTide').html (lowTideTime.getHours() + 1 + ":" + (lowTideTime.getMinutes()<10?'0':'') + lowTideTime.getMinutes());
-                            //     }
-                    
-                            //     if (data.extremes[1].type == 'Low') {
-                            //         //document.querySelector ('lowTide').innerHTML = data.extremes[1].date;
-                            //         lowTideTime = new Date (data.extremes[1].date)
-                            //         $('.lowTide').html (lowTideTime.getHours() + 1 + ":" + (lowTideTime.getMinutes()<10?'0':'') + lowTideTime.getMinutes());
-                            //     }
-                            //     else {
-                            //         //document.querySelector ('highTide').innerHTML = data.extremes[1].date;
-                            //         highTideTime = new Date (data.extremes[1].date)
-                            //         $('.highTide').html (highTideTime.getHours() + 1 + ":" + (highTideTime.getMinutes()<10?'0':'') + highTideTime.getMinutes());
-                            //     }
-                    
-                            //     calculateHeight (new Date (), lowTideTime, highTideTime);
-                            // }
-                            // else {
-                            //     $('.highTide').html ("Error getting tide data.");
-                            // }
-                            //$('.highTide').html (data.extremes[0].date.toString());
                             if (data.extremes[0].date !== NaN) {
                                 if (data.extremes[0].type === 'High') {
-                                    //document.querySelector ('highTide').innerHTML = data.extremes[0].date;
-                                    highTideTime = new Date (data.extremes[0].date.replace(/\s/, 'T'))
-                                    $('.highTide').html (highTideTime.getHours() + ":" +  highTideTime.getMinutes());
+                                    highTideTime = new Date (data.extremes[0].date.replace(/\s/, 'T'));
+                                    $('.highTide').html ((highTideTime.getHours()<10?'0':'') + highTideTime.getHours() + ":" + (highTideTime.getMinutes()<10?'0':'') + highTideTime.getMinutes());
                                 }
                                 else {
-                                    //document.querySelector ('lowTide').innerHTML = data.extremes[0].date;
-                                    lowTideTime = new Date (data.extremes[0].date.replace(/\s/, 'T'))
-                                    $('.lowTide').html (lowTideTime.getHours() +  ":" + lowTideTime.getMinutes());
+                                    lowTideTime = new Date (data.extremes[0].date.replace(/\s/, 'T'));
+                                    $('.lowTide').html ((lowTideTime.getHours()<10?'0':'') + lowTideTime.getHours() + ":" + (lowTideTime.getMinutes()<10?'0':'') + lowTideTime.getMinutes());
                                 }
                     
                                 if (data.extremes[1].type == 'Low') {
-                                    //document.querySelector ('lowTide').innerHTML = data.extremes[1].date;
-                                    lowTideTime = new Date (data.extremes[1].date.replace(/\s/, 'T'))
-                                    $('.lowTide').html (lowTideTime.getHours() + ":" + lowTideTime.getMinutes());
+                                    lowTideTime = new Date (data.extremes[1].date.replace(/\s/, 'T'));
+                                    $('.lowTide').html ((lowTideTime.getHours()<10?'0':'') + lowTideTime.getHours() + ":" + (lowTideTime.getMinutes()<10?'0':'') + lowTideTime.getMinutes());
                                 }
                                 else {
-                                    //document.querySelector ('highTide').innerHTML = data.extremes[1].date;
-                                    highTideTime = new Date (data.extremes[1].date.replace(/\s/, 'T'))
-                                    $('.highTide').html (highTideTime.getHours() + ":" + highTideTime.getMinutes());
+                                    highTideTime = new Date (data.extremes[1].date.replace(/\s/, 'T'));
+                                    $('.highTide').html ((highTideTime.getHours()<10?'0':'') + highTideTime.getHours() + ":" + (highTideTime.getMinutes()<10?'0':'') + highTideTime.getMinutes());
                                 }
                     
                                 calculateHeight (new Date (), lowTideTime, highTideTime);
@@ -103,36 +70,6 @@ $(document).ready (() => {
     }
 
     let dateTime = new Date();
-    //document.querySelector ('time').innerHTML = dateTime
-    // $('.time').html (`${dateTime.getDate()}/${dateTime.getMonth()}/${dateTime.getFullYear()} ${(dateTime.getHours()<10?'0':'') + dateTime.getHours()}:${(dateTime.getMinutes()<10?'0':'') + dateTime.getMinutes()}`)
-
-    //.done(function(data) {
-    //       console.log (data)
-    //         if (data.extremes[0].type == 'High') {
-    //             //document.querySelector ('highTide').innerHTML = data.extremes[0].date;
-    //             highTideTime = new Date (data.extremes[0].date)
-    //             $('.highTide').html (highTideTime.getHours() + 1 + ":" + (highTideTime.getMinutes()<10?'0':'') + highTideTime.getMinutes());
-    //         }
-    //         else {
-    //             //document.querySelector ('lowTide').innerHTML = data.extremes[0].date;
-    //             lowTideTime = new Date (data.extremes[0].date)
-    //             $('.lowTide').html (lowTideTime.getHours() + 1 + ":" + (lowTideTime.getMinutes()<10?'0':'') + lowTideTime.getMinutes());
-    //         }
-
-    //         if (data.extremes[1].type == 'Low') {
-    //             //document.querySelector ('lowTide').innerHTML = data.extremes[1].date;
-    //             lowTideTime = new Date (data.extremes[1].date)
-    //             $('.lowTide').html (lowTideTime.getHours() + 1 + ":" + (lowTideTime.getMinutes()<10?'0':'') + lowTideTime.getMinutes());
-    //         }
-    //         else {
-    //             //document.querySelector ('highTide').innerHTML = data.extremes[1].date;
-    //             highTideTime = new Date (data.extremes[1].date)
-    //             $('.highTide').html (highTideTime.getHours() + 1 + ":" + (highTideTime.getMinutes()<10?'0':'') + highTideTime.getMinutes());
-    //         }
-
-    //         calculateHeight (new Date (), lowTideTime, highTideTime);
-    //     });
-    //     .fa
 })
 
 function calculateHeight (currentTime, nextLowTide, nextHighTide) {
